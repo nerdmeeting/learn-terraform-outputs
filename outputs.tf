d82f2832711a8447955c21b85f9f33a3debf077d
Nathan Bills <nathan@nerdmeeting.com>
2024-12-26 17:35:19 -0700

    Add new outputs lb_url and web_server_count.

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "vpc_id" {
   description = "ID of the project VPC"
   value = module.vpc.vpc_id
 }
+
+output "lb_url" {
+  description = "URL of the load balancer"
+  value = "http://${module.elb_http.elb_dns_name}/"
+}
+
+output "web_server_count" {
+  description = "Number of web servers provisioned"
+  value = length(module.ec2_instances.instance_ids)
+}
